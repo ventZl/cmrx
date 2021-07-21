49c3cdc33f5287279a6448b8cd495f84f6b6161e
Eduard Drusa <ventyl86@netkosice.sk>
2021-07-21 15:58:42 +0200

    Implement type checking for RPC calls
    * added: if rpc_call is performed, type check call arguments

--- include/cmrx/ipc/rpc.h
+++ include/cmrx/ipc/rpc.h
@@ -10,13 +10,45 @@
 #define RPC_GET_ARG_COUNT_HELPER(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, N, ...)	N
 #define RPC_GET_ARG_COUNT(...)			RPC_GET_ARG_COUNT_HELPER(__VA_ARGS__ __VA_OPT__(,) 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
 
-#define RPC_PASTER(argcount)	RPC_CALL_ ## argcount
-#define RPC_EVALUATOR(argcount)	RPC_PASTER(argcount)
+#define RPC_PASTER(argcount)	            RPC_CALL_ ## argcount
+#define RPC_EVALUATOR(argcount)	            RPC_PASTER(argcount)
 #define RPC_CALL_4(si, mi, _0, _1, _2, _3)	_rpc_call((unsigned) _0, (unsigned) _1, (unsigned) _2, (unsigned) _3, si, mi, 0xAA55AA55)
 #define RPC_CALL_3(si, mi, _0, _1, _2)		_rpc_call((unsigned) _0, (unsigned) _1, (unsigned) _2, 0, si, mi, 0xAA55AA55)
 #define RPC_CALL_2(si, mi, _0, _1)			_rpc_call((unsigned) _0, (unsigned) _1, 0, 0, si, mi, 0xAA55AA55)
 #define RPC_CALL_1(si, mi, _0)				_rpc_call((unsigned) _0, 0, 0, 0, si, mi, 0xAA55AA55)
 #define RPC_CALL_0(si, mi)					_rpc_call(0, 0, 0, 0, si, mi, 0xAA55AA55)
+
+#if 0
+/** Prototype implementation of typechecker requiring C11 support.
+ * It works, but is too strict in most cases. Left here as a curious
+ * piece of preprocessor magic. 
+ **/
+#define RPC_CHECK_PASTER(argcount, object, ...)     RPC_CHECK_TYPE_ ## argcount(object, __VA_ARGS__)
+#define RPC_TYPECHECKER(argcount, object, ...)      RPC_CHECK_PASTER(argcount, object, __VA_ARGS__)
+#define RPC_CHECK_TYPE_4(object, _0, _1, _2, _3)    RPC_CHECK_TYPE(object, (void *, __typeof__(_0), __typeof__(_1), __typeof__(_2), __typeof__(_3)))
+#define RPC_CHECK_TYPE_3(object, _0, _1, _2)        RPC_CHECK_TYPE(object, (void *, __typeof__(_0), __typeof__(_1), __typeof__(_2)))
+#define RPC_CHECK_TYPE_2(object, _0, _1)            RPC_CHECK_TYPE(object, (void *, __typeof__(_0), __typeof__(_1)))
+#define RPC_CHECK_TYPE_1(object, _0)                RPC_CHECK_TYPE(object, (void *, __typeof__(_0)))
+#define RPC_CHECK_TYPE_0(object, foo)               RPC_CHECK_TYPE(object, (void *))
+
+#define RPC_CHECK_TYPE(object, typeinfo)    _Static_assert(_Generic((object),\
+											void (*)typeinfo : 1,\
+											int (*)typeinfo : 1,\
+											int * (*)typeinfo : 1,\
+											default : 0), "Type mismatch performing RPC call!");
+#else
+
+#define RPC_TYPECHECKER(argcount, object, ...)      if (0) { object((void *) 0 __VA_OPT__(,) __VA_ARGS__); }
+
+#define RPC_CHECK_TYPE_4(object, _0, _1, _2, _3)	RPC_CHECK_TYPE(object, (_0, _1, _2, _3))
+#define RPC_CHECK_TYPE_3(object, _0, _1, _2)		RPC_CHECK_TYPE(object, (_0, _1, _2))
+#define RPC_CHECK_TYPE_2(object, _0, _1)			RPC_CHECK_TYPE(object, (_0, _1))
+#define RPC_CHECK_TYPE_1(object, _0)				RPC_CHECK_TYPE(object, (_0))
+#define RPC_CHECK_TYPE_0(object, foo)				RPC_CHECK_TYPE(object, ())
+
+#define RPC_CHECK_TYPE(object, args)				if (0) { object ## args }
+
+#endif
 
 /**
  * @ingroup api_rpc
@@ -28,6 +60,11 @@
  * Calls service published by different thread and/or process. Service can take 0 to 4
  * arguments, which are passed down to it. Return value of service is passed back to the
  * caller.
+ * Services are represented by structures allocated in service provider thread. These 
+ * structures have to contain pointer to their virtual method table as their first member.
+ * Virtual method table is then queried for presence of method name used to perform the call.
+ * If such method exists, then arguments used to perform the call are checked against
+ * method prototype.
  * @param service_instance address of service instance, which is being called
  * @param method_name name of method within service, which has to be called
  * @returns whatever value service returned
@@ -36,7 +73,8 @@
 	RPC_EVALUATOR(RPC_GET_ARG_COUNT(__VA_ARGS__))(\
 			(service_instance), \
 			offsetof(typeof(*((service_instance)->vtable)), method_name) / sizeof(void *), \
-			##__VA_ARGS__)
+			##__VA_ARGS__);\
+	RPC_TYPECHECKER(RPC_GET_ARG_COUNT(__VA_ARGS__), (service_instance)->vtable->method_name, __VA_ARGS__)
 
 /** Internal implementation of remote procedure call in userspace.
  *
