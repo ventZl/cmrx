775867d6bd75b83e6f4d268d7f842f64c7d7e9c9
Eduard Drusa <ventyl86@netkosice.sk>
2023-07-19 23:37:55 +0200

    Add tests testing creating new threads with various priority
    These two tests test if creation of new thread and manual calling of
    sched_yield() will give control to the right thread. Thread with the
    highest priority ready to run shall be executed().

---
+++ src/testsuite/thread_create_high_prio/init.c
@@ -0,0 +1,22 @@
+#include <cmrx/application.h>
+#include <cmrx/ipc/thread.h>
+#include <debug.h>
+
+int new_thread_entry(void * data)
+{
+    TEST_SUCCESS();
+    return 0;
+}
+
+int init_main(void * data)
+{
+    thread_create(new_thread_entry, NULL, 32);
+    sched_yield();
+	TEST_FAIL();
+	return 0;
+}
+
+OS_APPLICATION_MMIO_RANGE(thread_create_high_prio_init, 0x40000000, 0x60000000);
+OS_APPLICATION(thread_create_high_prio_init);
+OS_THREAD_CREATE(thread_create_high_prio_init, init_main, NULL, 64);
+
