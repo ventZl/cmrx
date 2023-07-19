e1db96d2c16d75de7bd8921ed89ae4557f413282
Eduard Drusa <ventyl86@netkosice.sk>
2023-07-19 22:59:37 +0200

    Test if RPC method returns correctly
    This test tests if control is passed to the calling program once
    RPC method returns.

---
+++ src/testsuite/rpc_return/callee.c
@@ -0,0 +1,28 @@
+#include <cmrx/application.h>
+#include <cmrx/ipc/thread.h>
+#include <debug.h>
+#include <cmrx/rpc/interface.h>
+#include "service.h"
+
+#include <cmrx/rpc/implementation.h>
+
+IMPLEMENTATION_OF(struct Service, struct ServiceVTable);
+
+uint32_t service_method(INSTANCE(this), uint32_t arg1)
+{
+    this->value = arg1;
+    return arg1;
+}
+
+VTABLE struct ServiceVTable service_vtable = {
+    service_method
+};
+
+struct Service service = {
+    &service_vtable,
+    0
+};
+
+OS_APPLICATION_MMIO_RANGE(rpc_return_callee, 0x40000000, 0x60000000);
+OS_APPLICATION(rpc_return_callee);
+
