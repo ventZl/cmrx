e67f2c15a347db553e529f7f1dbdec20370fc286
Eduard Drusa <ventyl86@netkosice.sk>
2023-10-17 11:55:29 +0200

    Test if runtime priority change work correctly.
    This test launches one statically defined thread which then creates thread of
    priority slightly lower than the original thread. First it is tested that the
    scheduler won't schedule lower priority thread. Then it is tested that after
    the original thread decreases its priority, the other thread is scheduled
    automatically.

---
+++ src/testsuite/setpriority/init.c
@@ -0,0 +1,26 @@
+#include <cmrx/application.h>
+#include <cmrx/ipc/thread.h>
+#include <debug.h>
+
+int new_thread_entry(void * data)
+{
+    TEST_SUCCESS();
+    while (1) {
+        // make infinite cycle here
+    }
+    return 0;
+}
+
+int init_main(void * data)
+{
+    uint8_t thread_id = thread_create(new_thread_entry, NULL, 66);
+    sched_yield();
+	TEST_STEP(1);
+    setpriority(128);
+	return 0;
+}
+
+OS_APPLICATION_MMIO_RANGE(setpriority_init, 0x40000000, 0x60000000);
+OS_APPLICATION(setpriority_init);
+OS_THREAD_CREATE(setpriority_init, init_main, NULL, 64);
+
