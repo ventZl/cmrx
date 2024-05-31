6afd44076007280d6e08b711c5e7d9f16625e745
ventZl <ventyl86@netkosice.sk>
2024-05-31 21:52:41 +0000

    Deploying to gh-pages from @ ventZl/cmrx@6b6a4cc61144e354cb83984c2d39bde04029cd14 🚀

--- search/variables_e.js
+++ search/variables_e.js
@@ -5,6 +5,8 @@ var searchData=
   ['read_2',['read',['../structComSourceVMT.html#afa8d81d3914e95471ab4d927a5296970',1,'ComSourceVMT::read'],['../structComChannelVMT.html#af92f2222ca758d8c04449c9f7a0e7dab',1,'ComChannelVMT::read']]],
   ['readable_5fnotify_3',['readable_notify',['../structComNotificationVMT.html#a011e798d6130972953dd0c45bca156ce',1,'ComNotificationVMT']]],
   ['ready_4',['ready',['../structComSourceVMT.html#ae225575afd2d840418a51c1d93d497a0',1,'ComSourceVMT::ready'],['../structComChannelVMT.html#a38ab70634468ed478c2827397d2ac816',1,'ComChannelVMT::ready']]],
-  ['rpc_5finterface_5',['rpc_interface',['../structOS__process__definition__t.html#a60479915c51d1fceacf4ea6ca72cb415',1,'OS_process_definition_t']]],
-  ['rpc_5fstack_6',['rpc_stack',['../structOS__thread__t.html#ac38a8ca09c98ad92cecefcd716534920',1,'OS_thread_t']]]
+  ['reg1_5',['REG1',['../structMPU__Registers.html#a1743318dffbc94c876f6726ae2c8b46c',1,'MPU_Registers']]],
+  ['reg2_6',['REG2',['../structMPU__Registers.html#a0668dd1313f2045221b5e32512cd21c2',1,'MPU_Registers']]],
+  ['rpc_5finterface_7',['rpc_interface',['../structOS__process__definition__t.html#a60479915c51d1fceacf4ea6ca72cb415',1,'OS_process_definition_t']]],
+  ['rpc_5fstack_8',['rpc_stack',['../structOS__thread__t.html#ac38a8ca09c98ad92cecefcd716534920',1,'OS_thread_t']]]
 ];
--- structOS__thread__t.js
+++ structOS__thread__t.js
@@ -7,7 +7,7 @@ var structOS__thread__t =
     [ "rpc_stack", "structOS__thread__t.html#ac38a8ca09c98ad92cecefcd716534920", null ],
     [ "signal_handler", "structOS__thread__t.html#a6e3cdaf8f549f44f9dbbb9450c00fe67", null ],
     [ "signals", "structOS__thread__t.html#a07cea510f30eafa336c112e630cb2542", null ],
-    [ "sp", "structOS__thread__t.html#ab6d0248c868151b060efcdada2aa7898", null ],
+    [ "sp", "structOS__thread__t.html#a402b571dc6672000a6f56eccff94b78e", null ],
     [ "stack_id", "structOS__thread__t.html#a7ea5132f358b5e6003cf6c480b1f9177", null ],
     [ "state", "structOS__thread__t.html#a3316dfeb2de5d7b03f7e0f6842f7c256", null ]
 ];
