f7cc1ed0f4c1aa4a4ffe9c746c26b0d396c528c3
Eduard Drusa <ventyl86@netkosice.sk>
2024-06-01 20:47:34 +0200

    Add tests for timer / scheduler interaction
    Scheduler itself is tested elsewhere, so is timer infrastructure.
    The cooperation of them is tested here.

--- src/os/arch/testing/pendsv.c
+++ src/os/arch/testing/pendsv.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include <cmrx/os/runtime.h>
 
 bool schedule_context_switch_called = false;
 
@@ -8,6 +9,14 @@ bool schedule_context_switch(uint32_t current_task, uint32_t next_task)
     (void) current_task;
     (void) next_task;
     schedule_context_switch_called = true;
+	if (os_threads[current_task].state == THREAD_STATE_RUNNING)
+	{
+		// only mark leaving thread as ready, if it was runnig before
+		// if leaving thread was, for example, quit before calling
+		// os_sched_yield, then this would return it back to life
+		os_threads[current_task].state = THREAD_STATE_READY;
+	}
+    os_threads[next_task].state = THREAD_STATE_RUNNING;
 	return true;
 }
 
---
+++ src/os/kernel/tests/os_sched_timing_callback.c
@@ -0,0 +1,97 @@
+#include <cmrx/os/sched.h>
+#include <cmrx/os/timer.h>
+#include <cmrx/os/signal.h>
+#include <cmrx/ipc/signal.h>
+#include <ctest.h>
+#include <string.h>
+#include <arch/corelocal.h>
+
+struct TimerEntry_t {
+	uint32_t sleep_from;      ///< time at which sleep has been requested
+	uint32_t interval;        ///< amount of time sleep shall take
+	uint8_t thread_id;        ///< thread ID which requested the sleep
+};
+
+// Kernel private functions and variables, not part of any header
+extern struct OS_core_state_t core[OS_NUM_CORES];
+extern struct TimerEntry_t sleepers[SLEEPERS_MAX];
+
+CTEST_DATA(os_sched_timing_callback) {
+};
+
+CTEST_SETUP(os_sched_timing_callback) {
+    memset(&os_threads, 0, sizeof(os_threads));
+    core[0].thread_current = 0;
+    memset(&sleepers, 0, sizeof(sleepers));
+}
+
+CTEST2(os_sched_timing_callback, round_robin_same_prio) {
+    os_threads[0].state = THREAD_STATE_RUNNING;
+    os_threads[1].state = THREAD_STATE_READY;
+
+    os_threads[0].priority = 32;
+    os_threads[1].priority = 32;
+
+    os_sched_timing_callback(25);
+
+    ASSERT_EQUAL(core[0].thread_current, 1);
+    ASSERT_EQUAL(os_threads[0].state, THREAD_STATE_READY);
+    ASSERT_EQUAL(os_threads[1].state, THREAD_STATE_RUNNING);
+
+    os_sched_timing_callback(25);
+
+    ASSERT_EQUAL(core[0].thread_current, 0);
+    ASSERT_EQUAL(os_threads[0].state, THREAD_STATE_RUNNING);
+    ASSERT_EQUAL(os_threads[1].state, THREAD_STATE_READY);
+}
+
+CTEST2(os_sched_timing_callback, one_thread) {
+    os_threads[0].state = THREAD_STATE_RUNNING;
+
+    os_threads[0].priority = 32;
+
+    os_sched_timing_callback(25);
+
+    ASSERT_EQUAL(core[0].thread_current, 0);
+    ASSERT_EQUAL(os_threads[0].state, THREAD_STATE_RUNNING);
+}
+
+CTEST2(os_sched_timing_callback, set_itimer) {
+    os_threads[0].state = THREAD_STATE_RUNNING;
+    os_threads[1].state = THREAD_STATE_READY;
+
+    os_threads[0].priority = 32;
+    os_threads[1].priority = 48;
+
+    os_sched_timing_callback(2500);
+
+    ASSERT_EQUAL(core[0].thread_current, 0);
+    ASSERT_EQUAL(os_threads[0].state, THREAD_STATE_RUNNING);
+
+    int tim_ret = os_setitimer(2500);
+
+    ASSERT_EQUAL(tim_ret, 0);
+
+    int thr_ret = os_kill(0, SIGSTOP);
+    ASSERT_EQUAL(thr_ret, 0);
+
+    os_sched_timing_callback(2400);
+    ASSERT_EQUAL(core[0].thread_current, 1);
+    ASSERT_EQUAL(os_threads[1].state, THREAD_STATE_RUNNING);
+
+    os_sched_timing_callback(200);
+    ASSERT_EQUAL(core[0].thread_current, 0);
+    ASSERT_EQUAL(os_threads[0].state, THREAD_STATE_RUNNING);
+
+    thr_ret = os_kill(0, SIGSTOP);
+    ASSERT_EQUAL(thr_ret, 0);
+
+    os_sched_timing_callback(2300);
+    ASSERT_EQUAL(core[0].thread_current, 1);
+    ASSERT_EQUAL(os_threads[1].state, THREAD_STATE_RUNNING);
+
+    os_sched_timing_callback(200);
+    ASSERT_EQUAL(core[0].thread_current, 0);
+    ASSERT_EQUAL(os_threads[0].state, THREAD_STATE_RUNNING);
+
+}
