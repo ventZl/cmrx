5f3c51a7b0712765dc412f054175942a281bb694
Eduard Drusa <ventyl86@netkosice.sk>
2024-07-30 07:35:46 +0200

    Make main entry of HIL tests runnable on SMP configurations
    On SMP configurations, enforce HIL tests to run on core 0.

--- testsuite/main.c
+++ testsuite/main.c
@@ -1,11 +1,16 @@
-#include <cmrx/os/sched.h>
+#include <cmrx/cmrx.h>
 #include <debug.h>
 #include <extra/systick.h>
+#include <conf/kernel.h>
 
 int main(void)
 {
     timing_provider_setup(1);
-	os_start();
+#ifdef CMRX_ARCH_SMP_SUPPORTED
+	os_start(0);
+#else
+    os_start();
+#endif
     TEST_FAIL();
     // This will never be called but it forces TEST_STEP to be 
     // linked into binary. This enables the generic GDB script to
