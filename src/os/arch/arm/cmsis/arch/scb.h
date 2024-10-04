872dc2c7e10413de56b9d4c069dcd4a30be15032
Eduard Drusa <ventyl86@netkosice.sk>
2024-10-04 14:25:13 +0200

    Fix incorrect placement of ARMv7M/8M defines
    Defines related to ARMv7M and ARMv8M need to be guarded by ifdef
    so they are not used in ARMv6M code.

--- src/os/arch/arm/cmsis/arch/scb.h
+++ src/os/arch/arm/cmsis/arch/scb.h
@@ -4,20 +4,21 @@
 
 #ifdef __ARM_ARCH_6M__
 
-#define SCB_CFSR 0
+#   define SCB_CFSR 0
 
 #else 
 
-#define SCB_CFSR (SCB->CFSR)
+#   define SCB_CFSR (SCB->CFSR)
+
+#   define SCB_ICSR (SCB->ICSR)
+
+#   define SCB_ICSR_PENDSVSET (SCB_ICSR_PENDSVSET_Msk)
+#   define SCB_CFSR_IACCVIOL (SCB_CFSR_IACCVIOL_Msk)
+#   define SCB_CFSR_DACCVIOL (SCB_CFSR_DACCVIOL_Msk)
+#   define SCB_CFSR_MMARVALID (SCB_CFSR_MMARVALID_Msk)
 
 #endif
 
-#define SCB_ICSR (SCB->ICSR)
-
-#define SCB_ICSR_PENDSVSET (SCB_ICSR_PENDSVSET_Msk)
-#define SCB_CFSR_IACCVIOL (SCB_CFSR_IACCVIOL_Msk)
-#define SCB_CFSR_DACCVIOL (SCB_CFSR_DACCVIOL_Msk)
-#define SCB_CFSR_MMARVALID (SCB_CFSR_MMARVALID_Msk)
 
 #define cortex_disable_interrupts __disable_irq
 #define cortex_enable_interrupts __enable_irq
