601dc35c325827e89e5032a0a63f9e3b4a8b0f80
Eduard Drusa <ventyl86@netkosice.sk>
2024-11-12 09:10:33 +0100

    Test for os_shutdown
    Test that the os_shutdown function does what it should:
    * disable interrupts
    * disable MPU
    * call to the cmrx_shutdown_handler
    
    The latter will use setjmp/longjmp to escape from the os_shutdown
    call tree and return back into the test.

--- src/os/arch/testing/mpu.c
+++ src/os/arch/testing/mpu.c
@@ -1,5 +1,9 @@
 #include <conf/kernel.h>
 #include <arch/mpu.h>
+#include <stdbool.h>
+
+bool memory_protection_stop_called = false;
+bool kernel_shutdown_called = false;
 
 void os_memory_protection_start()
 {
@@ -8,12 +12,18 @@ void os_memory_protection_start()
 
 void os_memory_protection_stop()
 {
+    memory_protection_stop_called = true;
+}
+
+__attribute__((weak)) void cmrx_shutdown_handler()
+{
 
 }
 
 void os_kernel_shutdown()
 {
-
+    kernel_shutdown_called = true;
+    cmrx_shutdown_handler();
 }
 
 int mpu_init_stack(int thread_id)
