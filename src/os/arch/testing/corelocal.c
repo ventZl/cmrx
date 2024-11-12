beedeaacbb8af79a77d65bea21382ba3f33aacc4
Eduard Drusa <ventyl86@netkosice.sk>
2024-11-12 09:09:04 +0100

    Add code to allow capturing of calls to os_core_lock/unlock
    We need this for testing.

--- src/os/arch/testing/corelocal.c
+++ src/os/arch/testing/corelocal.c
@@ -6,6 +6,9 @@
 unsigned cmrx_current_core = 0;
 unsigned cmrx_os_smp_locked = 0;
 
+unsigned cmrx_os_core_lock_called = 0;
+unsigned cmrx_os_core_unlock_called = 0;
+
 callback_t cmrx_smp_locked_callback = NULL;
 callback_t cmrx_smp_unlocked_callback = NULL;
 callback_t cmrx_smp_wrong_lock_callback = NULL;
@@ -14,6 +17,15 @@ unsigned coreid()
 { 
     return cmrx_current_core; 
 }
+
+void os_core_lock() {
+    cmrx_os_core_lock_called = 1;
+}
+
+void os_core_unlock() {
+    cmrx_os_core_unlock_called = 1;
+}
+
 
 void os_smp_lock() { 
     if (cmrx_os_smp_locked)
