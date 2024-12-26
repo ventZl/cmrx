6c66f13cb1025b1acbd4855a21ab3a254dbd8fca
Eduard Drusa <ventyl86@netkosice.sk>
2024-12-26 13:47:59 +0100

    Fix unit tests for queue server
    * fixed: Allocate data storage for the queue
    * changed: Too large allocation unit test is skipped as current API does
      not allow this to happen.

--- src/extra/queue_server/tests/test_queue_server.c
+++ src/extra/queue_server/tests/test_queue_server.c
@@ -2,7 +2,7 @@
 #include <extra/queue_server/queue.h>
 #include <string.h>
 
-static struct Queue queue;
+STATIC_QUEUE(queue, 256);
 
 #define TEST_QUEUE_DEPTH 16
 #define TEST_QUEUE_ITEM_SIZE 12
@@ -16,21 +16,21 @@ CTEST_DATA(queue_server) {
 };
 
 CTEST_SETUP(queue_server) {
-    bool rv = queue_init(&queue, TEST_QUEUE_DEPTH, TEST_QUEUE_ITEM_SIZE);
+    bool rv = queue_init(&queue.q, TEST_QUEUE_DEPTH, TEST_QUEUE_ITEM_SIZE);
     ASSERT_EQUAL(rv, true);
 }
 
 CTEST2(queue_server, queue_empty_read) {
     unsigned char buffer[TEST_QUEUE_ITEM_SIZE];
-    bool rv = queue_receive(&queue, buffer);
+    bool rv = queue_receive(&queue.q, buffer);
 
     ASSERT_EQUAL(rv, false);
 }
 
-CTEST2(queue_server, queue_too_large_init_fail) {
+// Test skipped as currently this cannot be reasonably triggered
+CTEST2_SKIP(queue_server, queue_too_large_init_fail) {
     // make sure to initialize for too many items to be stored
-    unsigned queue_items = (QUEUE_LENGTH * 2) / TEST_QUEUE_ITEM_SIZE;
-    bool rv = queue_init(&queue, queue_items, TEST_QUEUE_ITEM_SIZE);
+    bool rv = queue_init(&queue.q, TEST_QUEUE_DEPTH * 2, TEST_QUEUE_ITEM_SIZE);
 
     ASSERT_EQUAL(rv, false);
 }
@@ -38,7 +38,7 @@ CTEST2(queue_server, queue_too_large_init_fail) {
 CTEST2(queue_server, queue_empty_write) {
     unsigned char buffer[TEST_QUEUE_ITEM_SIZE] = { 0x42 };
 
-    bool rv = queue_send(&queue, buffer);
+    bool rv = queue_send(&queue.q, buffer);
 
     ASSERT_EQUAL(rv, true);
 
@@ -51,11 +51,11 @@ CTEST2(queue_server, queue_write_read) {
     int cmp = memcmp(buffer, buffer2, TEST_QUEUE_ITEM_SIZE);
     ASSERT_NOT_EQUAL(cmp, 0);
 
-    bool rv = queue_send(&queue, buffer);
+    bool rv = queue_send(&queue.q, buffer);
 
     ASSERT_EQUAL(rv, true);
 
-    rv = queue_receive(&queue, buffer2);
+    rv = queue_receive(&queue.q, buffer2);
 
     ASSERT_EQUAL(rv, true);
 
@@ -67,11 +67,11 @@ CTEST2(queue_server, queue_depth_correct) {
     unsigned char buffer[TEST_QUEUE_ITEM_SIZE] = { 0x42 };
     bool rv;
     for (unsigned q = 0; q < TEST_QUEUE_DEPTH; ++q) {
-        rv = queue_send(&queue, buffer);
+        rv = queue_send(&queue.q, buffer);
         ASSERT_EQUAL(rv, true);
     }
 
-    rv = queue_send(&queue, buffer);
+    rv = queue_send(&queue.q, buffer);
     ASSERT_EQUAL(rv, false);
 }
 
@@ -81,21 +81,21 @@ CTEST2(queue_server, queue_max_depth_read) {
     unsigned char buffer_out[TEST_QUEUE_ITEM_SIZE] = { 0 };
     bool rv;
 
-    rv = queue_send(&queue, buffer_1st);
+    rv = queue_send(&queue.q, buffer_1st);
     ASSERT_EQUAL(rv, true);
 
     for (unsigned q = 1; q < TEST_QUEUE_DEPTH; ++q) {
-        rv = queue_send(&queue, buffer);
+        rv = queue_send(&queue.q, buffer);
         ASSERT_EQUAL(rv, true);
     }
 
-    rv = queue_receive(&queue, buffer_out);
+    rv = queue_receive(&queue.q, buffer_out);
     ASSERT_EQUAL(rv, true);
 
     int cmp = memcmp(buffer_1st, buffer_out, TEST_QUEUE_ITEM_SIZE);
     ASSERT_EQUAL(cmp, 0);
 
-    rv = queue_receive(&queue, buffer_out);
+    rv = queue_receive(&queue.q, buffer_out);
     ASSERT_EQUAL(rv, true);
 
     cmp = memcmp(buffer, buffer_out, TEST_QUEUE_ITEM_SIZE);
@@ -108,21 +108,21 @@ CTEST2(queue_server, queue_read_until_empty) {
     bool rv;
 
     for (unsigned q = 0; q < TEST_QUEUE_DEPTH; ++q) {
-        rv = queue_send(&queue, buffer);
+        rv = queue_send(&queue.q, buffer);
         ASSERT_EQUAL(rv, true);
     }
 
     for (unsigned q = 0; q < TEST_QUEUE_DEPTH; ++q) {
-        rv = queue_receive(&queue, buffer);
+        rv = queue_receive(&queue.q, buffer);
         ASSERT_EQUAL(rv, true);
     }
 
-    rv = queue_receive(&queue, buffer);
+    rv = queue_receive(&queue.q, buffer);
     ASSERT_EQUAL(rv, false);
 }
 
 CTEST2(queue_server, queue_empty_initially) {
-    bool rv = queue_empty(&queue);
+    bool rv = queue_empty(&queue.q);
     ASSERT_EQUAL(rv, true);
 }
 
@@ -131,9 +131,9 @@ CTEST2(queue_server, queue_not_empty_after_write) {
 
     bool rv;
 
-    rv = queue_send(&queue, buffer);
+    rv = queue_send(&queue.q, buffer);
 
-    rv = queue_empty(&queue);
+    rv = queue_empty(&queue.q);
     ASSERT_EQUAL(rv, false);
 }
 
@@ -142,15 +142,15 @@ CTEST2(queue_server, queue_empty_after_write_read) {
 
     bool rv;
 
-    rv = queue_send(&queue, buffer);
-    rv = queue_receive(&queue, buffer);
+    rv = queue_send(&queue.q, buffer);
+    rv = queue_receive(&queue.q, buffer);
 
-    rv = queue_empty(&queue);
+    rv = queue_empty(&queue.q);
     ASSERT_EQUAL(rv, true);
 }
 
 CTEST2(queue_server, queue_not_full_initially) {
-    bool rv = queue_full(&queue);
+    bool rv = queue_full(&queue.q);
     ASSERT_EQUAL(rv, false);
 }
 
@@ -160,10 +160,10 @@ CTEST2(queue_server, queue_full_when_full) {
     bool rv;
 
     for (unsigned q = 0; q < TEST_QUEUE_DEPTH; ++q) {
-        rv = queue_send(&queue, buffer);
+        rv = queue_send(&queue.q, buffer);
     }
 
-    rv = queue_full(&queue);
+    rv = queue_full(&queue.q);
     ASSERT_EQUAL(rv, true);
 }
 
@@ -173,11 +173,11 @@ CTEST2(queue_server, queue_not_full_when_not_full) {
     bool rv;
 
     for (unsigned q = 0; q < TEST_QUEUE_DEPTH; ++q) {
-        rv = queue_send(&queue, buffer);
+        rv = queue_send(&queue.q, buffer);
     }
 
-    rv = queue_receive(&queue, buffer);
+    rv = queue_receive(&queue.q, buffer);
 
-    rv = queue_full(&queue);
+    rv = queue_full(&queue.q);
     ASSERT_EQUAL(rv, false);
 }
