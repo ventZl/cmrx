0dfa72cbefc27dda024a59d8d5282a7c1f1b490e
Eduard Drusa <ventyl86@netkosice.sk>
2025-06-09 21:16:25 +0200

    Build system: Application creation is target-specific
    The way how application is defined (what happens, what targets are
    appended to the main application library, what kind of library) may be
    target-specific. Move this logic from main CMake file into
    per-architecture CMake files.

--- cmake/CMRX.cmake
+++ cmake/CMRX.cmake
@@ -27,20 +27,5 @@ else()
     endif()
 endif()
 
-## Add firmware application definition
-# This function is a wrapper around add_library(), which will augment the library to act 
-# as a CMRX application.
-function(add_application NAME)
-	add_library(${NAME} STATIC EXCLUDE_FROM_ALL ${ARGN})
-	set_property(TARGET ${NAME} PROPERTY CMRX_IS_APPLICATION 1)
-    get_property(CMRX_ROOT_DIR GLOBAL PROPERTY CMRX_ROOT_DIR)
-	target_compile_definitions(${NAME} PRIVATE -D APPLICATION_NAME=${NAME})
-    add_custom_command(TARGET ${NAME} POST_BUILD
-        COMMAND ${CMRX_ROOT_DIR}/ld/checkapp.sh 
-        ${CMAKE_CURRENT_BINARY_DIR}/$<TARGET_FILE_NAME:${NAME}>
-        ${NAME}_instance
-        COMMENT "Checking application soundness"
-    )
-endfunction()
 
 
--- cmake/arch/linux/posix/CMRX.cmake
+++ cmake/arch/linux/posix/CMRX.cmake
@@ -15,7 +15,16 @@ function(add_firmware FW_NAME)
 	target_link_options(${FW_NAME} PUBLIC -Wl,-Map=${FW_NAME}.map)
 endfunction()
 
-function(target_link_libraries TGT_NAME)
-    _target_link_libraries(${TGT_NAME} ${ARGN})
+function(target_add_applications TGT_NAME)
+    target_link_libraries(${TGT_NAME} ${ARGN})
 endfunction()
 
+## Add firmware application definition
+# This function is a wrapper around add_library(), which will augment the library to act
+# as a CMRX application.
+function(add_application NAME)
+	add_library(${NAME} OBJECT EXCLUDE_FROM_ALL ${ARGN})
+	set_property(TARGET ${NAME} PROPERTY CMRX_IS_APPLICATION 1)
+    get_property(CMRX_ROOT_DIR GLOBAL PROPERTY CMRX_ROOT_DIR)
+	target_compile_definitions(${NAME} PRIVATE -D APPLICATION_NAME=${NAME})
+endfunction()
