83895f24b78651dfebe478f7bc1aa2260ee06a2c
Eduard Drusa <ventyl86@netkosice.sk>
2025-06-09 17:33:44 +0200

    Kernel: Rework the syscall calling convention
    Update the way how porting layers are supposed to define the syscall.
    
    The difference is that old way only communicated the syscall ID to the
    porting layer. New way provides a way how to communicate arguments as
    well. It is up on the porting layer if it uses this piece of information
    or not.

--- src/lib/notify.c
+++ src/lib/notify.c
@@ -4,12 +4,12 @@
 __SYSCALL int notify_object(const void * object)
 {
 	(void) object;
-	__SVC(SYSCALL_NOTIFY_OBJECT);
+	__SVC(SYSCALL_NOTIFY_OBJECT, object);
 }
 
 __SYSCALL int wait_for_object(const void * object, uint32_t timeout)
 {
 	(void) object;
 	(void) timeout;
-	__SVC(SYSCALL_WAIT_FOR_OBJECT);
+	__SVC(SYSCALL_WAIT_FOR_OBJECT, object, timeout);
 }
--- src/lib/thread.c
+++ src/lib/thread.c
@@ -17,25 +17,25 @@ __SYSCALL int thread_create(int (*entrypoint)(void *), void * data, uint8_t prio
     (void) entrypoint;
     (void) data;
     (void) priority;
-	__SVC(SYSCALL_THREAD_CREATE);
+	__SVC(SYSCALL_THREAD_CREATE, entrypoint, data, priority);
 }
 
 __SYSCALL int thread_join(int thread)
 {
     (void) thread;
-	__SVC(SYSCALL_THREAD_JOIN);
+	__SVC(SYSCALL_THREAD_JOIN, thread);
 }
 
 __SYSCALL int thread_exit(int status)
 {
     (void) status;
-	__SVC(SYSCALL_THREAD_EXIT);
+	__SVC(SYSCALL_THREAD_EXIT, status);
 }
 
 __SYSCALL int setpriority(uint8_t priority)
 {
     (void) priority;
-	__SVC(SYSCALL_SETPRIORITY);
+	__SVC(SYSCALL_SETPRIORITY, priority);
 }
 
 /** @ingroup api_thread
@@ -50,7 +50,7 @@ __SYSCALL int setpriority(uint8_t priority)
  * entrypoint to be recorded as thread return value.
  * @param arg0 value returned by thread entrypoint
  */
-void os_thread_dispose(int arg0)
+int os_thread_dispose(int arg0)
 {
     (void) arg0;
 	// Do not place anything here. It will clobber R0 value!
@@ -58,7 +58,7 @@ void os_thread_dispose(int arg0)
 	// Normally, call to thread_exit would be here. But as we know that the way which
 	// led to os_thread_dispose being called results into R0 holding arg0, we may call
 	// syscall directly.
-	__SVC(SYSCALL_THREAD_EXIT);
+	__SVC(SYSCALL_THREAD_EXIT, arg0);
 	//thread_exit(arg0);
 	ASSERT(0);
 	// this should be called when thread returns
