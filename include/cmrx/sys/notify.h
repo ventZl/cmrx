1362ab80a182a7b72a0b3aaf7cf5580eea14cc10
Eduard Drusa <ventyl86@netkosice.sk>
2025-09-04 12:47:09 +0200

    refactor: Change flag defs from macros to enum
    This makes flag definition less prone to assigning two flags the same
    value and then wondering why the code behaves in such a weird way.
    
    feat: Add flag for incrementing value of object waited for
    
    Platforms that lack atomic ops support need to be able to perform futex
    state value incrementation in semi-atomic way (at least from thread's
    POV). Add definition for it.

--- include/cmrx/sys/notify.h
+++ include/cmrx/sys/notify.h
@@ -6,9 +6,12 @@
  * @{
  */
 
-#define NOTIFY_PRIORITY_INHERIT_FLAG        (1 << 0)
-#define NOTIFY_PRIORITY_DROP_FLAG           (1 << 1)
-#define NOTIFY_QUEUE_NOTIFICATION_FLAG      (1 << 2)
+enum NotifyFlags {
+    NOTIFY_PRIORITY_INHERIT_FLAG = 0,
+    NOTIFY_PRIORITY_DROP_FLAG,
+    NOTIFY_QUEUE_NOTIFICATION_FLAG,
+    NOTIFY_VALUE_INCREMENT_FLAG,
+};
 
 /** Inherit scheduler priority to specific thread if caller is going to be suspended.
  * This flag tells the kernel to inherit scheduler priority of the current thread to
@@ -17,26 +20,32 @@
  * of the calling thread then priority change does not happen.
  * @param thread_id ID of the thread to inherit the priority
  */
-#define NOTIFY_PRIORITY_INHERIT(thread_id) (((thread_id & 0xFF) << 8) | NOTIFY_PRIORITY_INHERIT_FLAG)
+#define NOTIFY_PRIORITY_INHERIT(thread_id) (((thread_id & 0xFF) << 8) | (1 << NOTIFY_PRIORITY_INHERIT_FLAG))
 
 /** Drop inherited priority after notification has been sent.
  * Calling thread will drop elevated priority after notifications have been sent and return
  * to its default priority.
  */
-#define NOTIFY_PRIORITY_DROP                (NOTIFY_PRIORITY_DROP_FLAG)
+#define NOTIFY_PRIORITY_DROP                (1 << NOTIFY_PRIORITY_DROP_FLAG)
 
 /** If there is no object waiting for notification then queue notification for later.
  * Enabling this flag will cause that any notification that won't immediately wake up
  * any thread will be queued into pending notifications list.
  */
-#define NOTIFY_QUEUE_NOTIFICATION           (NOTIFY_QUEUE_NOTIFICATION_FLAG)
+#define NOTIFY_QUEUE_NOTIFICATION           (1 << NOTIFY_QUEUE_NOTIFICATION_FLAG)
+
+/** If value is as expected, then increment it before returning from the syscall.
+ * This effectively implements compare-increment semantics.
+ */
+#define NOTIFY_VALUE_INCREMENT              (1 << NOTIFY_VALUE_INCREMENT_FLAG)
+
 /** Statement that there are no flags passed to the call.
  */
 #define NOTIFY_NO_FLAGS                     0
 
 #define NOTIFY_PRIORITY_INHERIT_THREAD(flags)   ((flags >> 8) && 0xFF)
 
-#define WAIT_FOR_OBJECT_FLAGS_MASK          (NOTIFY_PRIORITY_INHERIT_FLAG)
-#define NOTIFY_OBJECT_FLAGS_MASK            (NOTIFY_PRIORITY_DROP_FLAG | NOTIFY_QUEUE_NOTIFICATION)
+#define WAIT_FOR_OBJECT_FLAGS_MASK          ((1 << NOTIFY_PRIORITY_INHERIT_FLAG) | NOTIFY_VALUE_INCREMENT)
+#define NOTIFY_OBJECT_FLAGS_MASK            (NOTIFY_PRIORITY_DROP | NOTIFY_QUEUE_NOTIFICATION)
 
 /** @} */
