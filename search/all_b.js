58168b121fef59474a8489258aab9a5ae7166d13
ventZl <ventyl86@netkosice.sk>
2025-09-17 12:17:53 +0000

    Deploying to gh-pages from @ ventZl/cmrx@bb61a04c9f1166c7b38a6d57a2c2d86524875b8e 🚀

--- search/all_b.js
+++ search/all_b.js
@@ -1,14 +1,14 @@
 var searchData=
 [
   ['h_0',['h',['../group__arch__arch.html#corelocal_h',1,'corelocal.h'],['../group__arch__arch.html#mpu_h',1,'mpu.h'],['../group__arch__arch.html#sysenter_h',1,'sysenter.h']]],
-  ['hal_20tweaks_1',['CubeMX HAL tweaks',['../getting_started_cubemx.html#autotoc_md54',1,'']]],
+  ['hal_20tweaks_1',['CubeMX HAL tweaks',['../getting_started_cubemx.html#autotoc_md53',1,'']]],
   ['handler_2',['handler',['../structSyscall__Entry__t.html#ace65d18a0d600d65292f949c52b1937a',1,'Syscall_Entry_t']]],
   ['handling_3',['Signal handling',['../group__os__signal.html',1,'']]],
   ['hard_5ffault_5fhandler_4',['hard_fault_handler',['../group__arch__arm__mpu.html#ga80b8e496817c48aab711bab6cb8a148d',1,'mpu.c']]],
-  ['header_20directories_5',['Source and header directories',['../dev_env.html#autotoc_md60',1,'']]],
+  ['header_20directories_5',['Source and header directories',['../dev_env.html#autotoc_md61',1,'']]],
   ['headers_6',['C headers',['../group__arch__arch.html#headers',1,'']]],
   ['helper_7',['CMSIS helper',['../hal_integration.html#autotoc_md63',1,'']]],
-  ['high_20level_20design_20of_20rpc_20mechanism_8',['High level design of RPC mechanism',['../rpc_intro.html#autotoc_md32',1,'']]],
+  ['high_20level_20design_20of_20rpc_20mechanism_8',['High level design of RPC mechanism',['../rpc_intro.html#autotoc_md35',1,'']]],
   ['howto_3a_20integrating_20cmrx_20into_20cubemx_20project_9',['HOWTO: Integrating CMRX into CubeMX project',['../getting_started_cubemx.html',1,'getting_started']]],
   ['howto_3a_20integrating_20cmrx_20into_20pico_20sdk_20project_10',['HOWTO: Integrating CMRX into Pico-SDK project',['../getting_started_picosdk.html',1,'getting_started']]]
 ];
