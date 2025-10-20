11fe56654e6874843d2f85406b0f97572c0b3023
Eduard Drusa <ventyl86@netkosice.sk>
2025-10-20 09:54:56 +0200

    fix: Use CACHE entries for non-bool options
    Convert some newer non-bool option from option() to set(... CACHE).

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,11 +1,11 @@
 option(CMRX_ARCH_SMP_SUPPORTED "Architecture supports SMP and project is using it" OFF)
-option(CMRX_OS_NUM_CORES "Amount of cores present in CPU package" INT:1)
+set(CMRX_OS_NUM_CORES 1 CACHE STRING "Amount of cores present in CPU package")
 if (CMAKE_HOST_WIN32)
     set(UNIT_TESTS_DEFAULT OFF)
 else()
     set(UNIT_TESTS_DEFAULT ON)
 endif()
-option(CMRX_PLATFOM "Specific platform within architecture to build for" "generic")
+set(CMRX_PLATFORM "generic" CACHE STRING "Specific platform within architecture to build for")
 
 option(CMRX_UNIT_TESTS "Enable build of kernel unit tests" ${UNIT_TESTS_DEFAULT})
 option(CMRX_KERNEL_TRACING "Enable tracing of kernel events" OFF)
@@ -14,12 +14,12 @@ option(CMRX_KERNEL_TRANSACTION_VERIFICATION "Enable checking for nested transact
 option(CMRX_IDLE_THREAD_SHUTDOWN_CPU "Idle thread stops CPU" OFF)
 
 option(CMRX_CUSTOM_FLASH_RANGE "If set to ON then developer SHALL provide custom range that covers all readable and executable area. If set to OFF then kernel will determine this automatically based on linker file, if possible" OFF)
-option(CMRX_CUSTOM_FLASH_START "Custom start address of FLASH region. FLASH region includes this address" 0)
-option(CMRX_CUSTOM_FLASH_SIZE "Custom size of FLASH region" 0)
+set(CMRX_CUSTOM_FLASH_START 0 CACHE STRING "Custom start address of FLASH region. FLASH region includes this address")
+set(CMRX_CUSTOM_FLASH_SIZE 0 CACHE STRING "Custom size of FLASH region")
 
 option(CMRX_RPC_CANARY "Enable RPC canaries" OFF)
 option(CMRX_MAP_FILE_WITH_EXTENSION "MAP file contains full name of binary with ELF extension (.elf.map)" OFF)
-option(CMRX_CLANG_TIDY_LIBC_PATH "Path to standard C library used while linting" /usr/include)
+set(CMRX_CLANG_TIDY_LIBC_PATH /usr/include CACHE STRING "Path to standard C library used while linting")
 set(OS_STACK_SIZE 1024 CACHE STRING "Stack allocated per thread in bytes")
 set(OS_THREADS 8 CACHE STRING "Amount of entries in the thread table")
 set(OS_PROCESSES 8 CACHE STRING "Amount of entries in the process table")
@@ -30,6 +30,9 @@ set(CMRX_ALL_OPTIONS
     CMRX_ARCH_SMP_SUPPORTED
     CMRX_OS_NUM_CORES
     CMRX_KERNEL_TRACING
+    CMRX_CUSTOM_FLASH_RANGE
+    CMRX_CUSTOM_FLASH_START
+    CMRX_CUSTOM_FLASH_SIZE
     OS_STACK_SIZE
     OS_THREADS
     OS_PROCESSES
