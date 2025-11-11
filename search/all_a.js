78a39782ef25b41e1fce59bd561fe3fc0e4f1b0f
ventZl <ventyl86@netkosice.sk>
2025-11-11 14:50:07 +0000

    Deploying to gh-pages from @ ventZl/cmrx@261f444b4c5b3bc579f43d7b4ca85a439e6d5095 🚀

--- search/all_a.js
+++ search/all_a.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['generating_20project_20and_20getting_20cmrx_20source_0',['Generating project and getting CMRX source',['../getting_started_cubemx.html#autotoc_md37',1,'']]],
+  ['generating_20project_20and_20getting_20cmrx_20source_0',['Generating project and getting CMRX source',['../getting_started_cubemx.html#autotoc_md40',1,'']]],
   ['generic_20ipi_20provider_1',['Cortex-M generic IPI provider',['../group__aux__cm__ipi.html',1,'']]],
   ['get_5fcpu_5ffreq_2',['get_cpu_freq',['../group__api__standard.html#ga3237021168ac3f0aa996c3c97b51e323',1,'get_cpu_freq(void):&#160;std.c'],['../group__api__standard.html#ga3237021168ac3f0aa996c3c97b51e323',1,'get_cpu_freq(void):&#160;std.c']]],
   ['get_5fexception_5farg_5faddr_3',['get_exception_arg_addr',['../group__arch__arm__cortex.html#ga2ae6451ce266ac64b1839748e597ae87',1,'get_exception_arg_addr(ExceptionFrame *frame, unsigned argno, bool fp_active):&#160;cortex.c'],['../group__arch__arm__os.html#ga2ae6451ce266ac64b1839748e597ae87',1,'get_exception_arg_addr(ExceptionFrame *frame, unsigned argno, bool fp_active):&#160;cortex.c']]],
@@ -10,7 +10,7 @@ var searchData=
   ['get_5ftid_7',['get_tid',['../group__api__thread.html#ga52f86ad5da2364f17909fcb76bf84d71',1,'get_tid():&#160;thread.c'],['../group__api__thread.html#ga52f86ad5da2364f17909fcb76bf84d71',1,'get_tid():&#160;thread.c']]],
   ['get_5fvtable_5fprocess_8',['get_vtable_process',['../group__os__rpc.html#gab6a04c90d0385e2685fb5e494af298ff',1,'rpc.c']]],
   ['getmicrotime_9',['getmicrotime',['../group__api__standard.html#gab9090f01493a7759cdd3dd5bf4d917d2',1,'std.c']]],
-  ['getting_20cmrx_20source_10',['Generating project and getting CMRX source',['../getting_started_cubemx.html#autotoc_md37',1,'']]],
+  ['getting_20cmrx_20source_10',['Generating project and getting CMRX source',['../getting_started_cubemx.html#autotoc_md40',1,'']]],
   ['getting_20started_11',['Getting started',['../getting_started.html',1,'']]],
-  ['gpios_12',['Configuring GPIOs',['../getting_started_cubemx.html#autotoc_md35',1,'']]]
+  ['gpios_12',['Configuring GPIOs',['../getting_started_cubemx.html#autotoc_md39',1,'']]]
 ];
