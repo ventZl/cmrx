5e5c752918ee7c42bc6e933aa67e21ff6d2a6d3c
ventZl <ventyl86@netkosice.sk>
2025-11-12 12:25:19 +0000

    Deploying to gh-pages from @ ventZl/cmrx@07e31a3cfcf4ece280b22034754efba81df8df04 🚀

--- search/all_e.js
+++ search/all_e.js
@@ -2,11 +2,11 @@ var searchData=
 [
   ['last_5fpossible_5fevent_0',['last_possible_event',['../structTraceBuffer.html#a313796a790d9cc8da37d771f6b0a34b0',1,'TraceBuffer']]],
   ['length_1',['LENGTH',['../group__util.html#gafedd38df23e9c9f801b82b8cdf210dae',1,'util.h']]],
-  ['level_20design_20of_20rpc_20mechanism_2',['High level design of RPC mechanism',['../rpc_intro.html#autotoc_md33',1,'']]],
+  ['level_20design_20of_20rpc_20mechanism_2',['High level design of RPC mechanism',['../rpc_intro.html#autotoc_md30',1,'']]],
   ['libraries_3',['Libraries',['../group__libs.html',1,'']]],
   ['library_4',['Queue library',['../group__aux__queue.html',1,'']]],
-  ['linker_20script_20adjustment_20tool_5',['Linker script adjustment tool',['../dev_env.html#autotoc_md62',1,'']]],
-  ['linking_20cmrx_20to_20the_20project_6',['linking cmrx to the project',['../getting_started_cubemx.html#autotoc_md48',1,'Linking CMRX to the project'],['../getting_started_picosdk.html#autotoc_md69',1,'Linking CMRX to the project']]],
+  ['linker_20script_20adjustment_20tool_5',['Linker script adjustment tool',['../dev_env.html#autotoc_md58',1,'']]],
+  ['linking_20cmrx_20to_20the_20project_6',['linking cmrx to the project',['../getting_started_cubemx.html#autotoc_md52',1,'Linking CMRX to the project'],['../getting_started_picosdk.html#autotoc_md69',1,'Linking CMRX to the project']]],
   ['load_5fcontext_7',['LOAD_CONTEXT',['../group__arch__arm__cortex.html#ga4f0ecd71a6265dddd9f66c09cdf570fe',1,'cortex.h']]],
   ['log2_8',['LOG2',['../group__util.html#ga40805b5dd56ee36df59a7bbe403ddf33',1,'util.h']]],
   ['lr_9',['lr',['../structExceptionFrame.html#a00f9fffab2003c92039309a634072c93',1,'ExceptionFrame::lr'],['../structExceptionFrameFP.html#ae7fe95c677298cd9a968dcf66a3e213d',1,'ExceptionFrameFP::lr']]]
