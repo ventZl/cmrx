dda1a91ad5d6cbb6632eff950244f5271ee56469
ventZl <ventyl86@netkosice.sk>
2026-02-11 13:08:01 +0000

    Deploying to gh-pages from @ ventZl/cmrx@9093b38c288c0d65a7ee5565ccfd24694a98ce54 🚀

--- search/all_d.js
+++ search/all_d.js
@@ -6,7 +6,7 @@ var searchData=
   ['kernel_20events_3',['Tracing Kernel Events',['../group__os__tracing.html',1,'']]],
   ['kernel_20integration_20tests_4',['Kernel integration tests',['../md_HIL__TESTING.html#autotoc_md4',1,'']]],
   ['kernel_20runtime_20configuration_5',['Kernel runtime configuration',['../group__os__config.html',1,'']]],
-  ['kernel_20to_20use_20cubemx_6',['3. Configure the CMRX kernel to use CubeMX',['../getting_started_cubemx.html#autotoc_md56',1,'']]],
+  ['kernel_20to_20use_20cubemx_6',['3. Configure the CMRX kernel to use CubeMX',['../getting_started_cubemx.html#autotoc_md52',1,'']]],
   ['kernel_20to_20use_20pico_20sdk_7',['3. Configuring CMRX kernel to use Pico-SDK',['../getting_started_picosdk.html#autotoc_md75',1,'']]],
   ['kernel_20unit_20testing_8',['Kernel unit testing',['../md_UNIT__TESTING.html',1,'']]],
   ['kernel_5fexecute_5fthread_5fswitch_9',['kernel_execute_thread_switch',['../group__arch__linux__impl.html#gaf755bea0ee650e391e9ae2775b7131f4',1,'posix.c']]],
